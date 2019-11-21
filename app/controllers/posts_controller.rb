9b9ca7233bb0640be2e03f5087f782ce39452bfa
root <root@NURI_01.localdomain>
2019-11-22 00:51:06 +0100

    update category model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+    def index
+        @content_1='Sample blog body  1'
+        @content_2='Sample blog body  2'
+    end
+
+    def new
+    
+    end
+
+    def create
+    
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def new
+    
+    end
+
+    def destroy
+
+    end
+    
+end
